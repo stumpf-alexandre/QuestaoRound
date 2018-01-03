abe2675dd43fa58e3e62efe0db4e7bc286bf233f
Alexandre Stumpf <als.stumpf@gmail.com>
2018-01-03 01:19:37 -0200

    App feito em aula com auxilio do professor

---
+++ QuestaoRound/QuestaoRound.java
@@ -0,0 +1,9 @@
+/**
+ * Created by als on 02/08/2017.
+ */
+public class QuestaoRound {
+    public static void main(String[] args) {
+        double a = 3.14;
+        System.out.println(Math.max(Math.ceil(a),Math.floor(a)));
+    }
+}
